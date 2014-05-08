5d62fd53cb9835da46cda286a055eb657a2e3f0c
Naira <pnairai28@mail.ru>
2014-05-08 11:07:47 +0400

    online shop_eclips

---
+++ shop_eclips/src/online/store/MainActivity.java
@@ -0,0 +1,11 @@
+package online.store;
+
+public class MainActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shopper d = new Shopper();
+		d.shopper();
+	}
+
+}
---
+++ shop_eclips/src/online/store/Shoes.java
@@ -0,0 +1,12 @@
+package online.store;
+
+public class Shoes extends Product{
+
+public Shoes(){
+	setGin(6000);
+	setBrand("Gucci");
+	setQuality("Bark");
+	setCount(5);
+	}
+
+}
